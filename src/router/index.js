d67735dc22838eaf70a01d67f0588738ed8db3bb
hzpweb <745984360@qq.com>
2017-09-11 00:24:51 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Hello from '@/components/Hello'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Hello',
+//       component: Hello
+//     }
+//   ]
+// })
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Home from '@/components/tab/Home'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: '/Contact',
+      component: Contact
+    }
+  ]
+})
